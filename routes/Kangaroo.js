24b5156f9cba0777307e1d37f846675b60720f83
sri-123lakshmi <143051198+sri-123lakshmi@users.noreply.github.com>
2023-11-29 11:44:33 -0600

    add

--- routes/Kangaroo.js
+++ routes/Kangaroo.js
@@ -7,6 +7,15 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const Kangaroo_controlers= require('../controllers/Kangaroo');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+  if (req.user){
+  return next();
+  }
+  res.redirect("/login");
+  }
 /* GET Kangaroos */
 router.get('/', Kangaroo_controlers.Kangaroo_view_all_Page );
 
@@ -15,7 +24,7 @@ router.get('/detail', Kangaroo_controlers.Kangaroo_view_one_Page);
 /* GET create Kangaroo page */
 router.get('/create', Kangaroo_controlers.Kangaroo_create_Page);
 /* GET create update page */
-router.get('/update', Kangaroo_controlers.Kangaroo_update_Page);
+router.get('/update',secured,  Kangaroo_controlers.Kangaroo_update_Page);
 /* GET delete Kangaroo page */
 router.get('/delete', Kangaroo_controlers.Kangaroo_delete_Page);
 module.exports = router;
